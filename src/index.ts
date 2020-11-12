7725192df1e3953390030b10da6ac49083b77e19
RiyaParte <parteriya21@gmail.com>
2020-11-13 01:58:14 +0530

    adding infowindow

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
-//import {User} from './User';
-//import {Company} from './Company';
-new google.maps.Map(document.getElementById('map'),{
-    zoom: 1,
-    center: {
-        lat:0,
-        lng:0
-    }
-});
+import {User} from './User';
+import {Company} from './Company';
+import {CustomMap} from './CustomMap';
+new CustomMap('map');
+
+
+const user = new User();
+const company = new Company();
+const customMap = new CustomMap('map');
+
+customMap.addMarker(user);
+customMap.addMarker(company);
