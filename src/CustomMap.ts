a2f61e5298ff84cd085674968692e338b3ca367e
Richaparte <richa.parte@somaiya.edu>
2020-11-13 12:35:46 +0530

    modifying marker

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -3,7 +3,8 @@ interface Mappable{
     location: {
         lat: number,
         lng: number;
-    }
+    };
+    markerContent(): string;
 }
 
 export class CustomMap{
@@ -27,16 +28,16 @@ export class CustomMap{
             }
         });
 
+
+
         marker.addListener('click', () =>{
             const infoWindow = new google.maps.InfoWindow({
-                content: "Hi there!"
+                content: mappable.markerContent()
             });
 
             infoWindow.open(this.googleMap, marker);
         });
 
     }
-
-//hi
 
 }
